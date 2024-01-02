bc5cb80061d0dfb6f7803c891d275b5ac0b404f7
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2024-01-02 01:07:47 -0300

    basic player

--- app/src/main/java/com/example/music/app/MusicApp.kt
+++ app/src/main/java/com/example/music/app/MusicApp.kt
@@ -4,4 +4,13 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MusicApp() : Application()
+class MusicApp() : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        println("MusicApp.onCreate")
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+}
